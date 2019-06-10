0f1261b2cf3c482ea87afff43f0ae09e0a9f6b01
PeufOne <jonas.voisard@gmail.com>
2019-06-10 13:49:52 +0200

    app name automatique

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 var dev = process.env.NODE_ENV == 'development'
+var pkg = require('./package.json')
 
 module.exports = {
   apps : [{
-    name   : !dev ? 'app' : 'app-dev',
+    name   : !dev ? pkg.name : `${pkg.name}-dev`,
     script : "./app.js",
     watch: !dev ? false : ['app.js', 'config.js', 'utils.js', 'routes', 'models']
   }]
